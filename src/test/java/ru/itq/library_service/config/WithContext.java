8a5a7151f9a93632d98c3b76a75d455bb4b0f87e
k.sukhareva <k.sukhareva@itq-group.com>
2025-03-30 19:20:08 +0300

    Поправила настройку тестов

--- src/test/java/ru/itq/library_service/config/WithContext.java
+++ src/test/java/ru/itq/library_service/config/WithContext.java
@@ -5,12 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @EnableJpaRepositories(basePackages = {"ru.itq.library_service.repository"})
+@EmbeddedKafka(partitions = 1, topics = "record-queue")
+@TestPropertySource(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
 @ActiveProfiles("test")
 public class WithContext {
     @Autowired
